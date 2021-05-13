49ca320bf4cdc1b15b7a633ce1fbdea6909941b1
Pedro Lucas de Moliner de Castro <pedro.lucas.moliner.castro@gmail.com>
2021-05-13 13:59:39 -0400

    Renomeando os csv.c e csv.h para util.c e util.h

--- includes/util.h
+++ includes/util.h
@@ -1,9 +1,9 @@
 // Pedro Lucas de Moliner de Castro - 11795784
 // Matheus Henrique de Cerqueira Pinto - 11911104
 
-#ifndef CSV_H
-#define CSV_H
+#ifndef UTIL_H
+#define UTIL_H
 
 
 
-#endif // CSV_H
+#endif // UTIL_H
